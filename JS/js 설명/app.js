24a41bced8c50d649fb31db7d46feff13812228b
MaJaeYoung <dabeta1234@gmail.com>
2022-09-13 17:05:39 +0900

    html css start
    html css

---
+++ JS/js 설명/app.js
@@ -0,0 +1,111 @@
+//console.log(12)
+// 데이터 타임
+// 1. number 숫자
+// 2. string 문자
+// 3. boolean 가짜와 진짜(ture)(false)
+// 4. null 값이없는
+// 5. symbol 고정된값
+// 6. undefined 미정의 값
+// 7. Bigint 롱롱인트랑 같음
+
+
+// number = 123
+// console.log(number);
+
+// string = "마재영";
+// console.log(string);
+
+// boolean = false;
+// console.log(boolean);
+
+// a= null;
+// console.log(a);
+
+
+////let, var,const
+
+// let a=1;
+// let b=2;
+
+// console.log(a+b)
+// console.log(a*b)
+// console.log(a-b)
+
+
+//let 재선언이 불가능 재할당은 가능
+//let num = 100;
+//let num = 200;
+// num =200;
+// console.log(num)
+
+
+//const 재선언 불가능 재할당도 불가능하다.
+// const num2 =500;
+// const num2 = 1000;
+// num2 = 1000;
+// console.log(num2)
+
+
+//verwo 재선언 재할당 가능
+
+// var num3=500;
+// var num3 = 1000;
+// console.log(num3)
+
+
+// const str1 = Symbol("so");
+// const str2 = Symbol("so"); //같지만 다른값
+
+// console.log(str1 == str2)
+
+// let a;
+// console.log(a)  
+
+//array
+// const array = [1,2,3,4,5]
+// console.log(array);
+// const str = ["ㄱ","ㄴ","ㄷ","ㄹ","ㅁ"];
+// console.log(str);
+// str.push("ㅇ");  //추가
+// console.log(str);
+
+
+// object
+
+// const player = {
+//     name: "마재영",
+//     age: 17,
+//     fat: true,
+
+// }
+// console.log(player.name);
+// console.log(player.age);
+// console.log(player.fat);
+
+
+//function
+
+// function number(num){
+//     console.log(num)
+// }
+
+// number(4);
+
+// function plus(a,b){
+//     console.log(a+b);
+// }
+
+// plus(2,3);
+
+
+// const player = {
+//     name: "재영",
+//     sayHello: function(name){
+//         console.log("hello" + name)
+//     }
+// }
+
+// player.sayHello("마재영");
+
+
+
