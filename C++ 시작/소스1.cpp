b0f912fd704338a19200163be525bc20cf24873d
MaJaeYoung <dabeta1234@gmail.com>
2022-09-19 15:24:00 +0900

    cpp class 1
    c++

--- C++ 시작/소스1.cpp
+++ C++ 시작/소스1.cpp
@@ -200,65 +200,138 @@
 //}
 
 // 시분초 cpp
+//#include <iostream>
+//using namespace std;
+//
+//class MyTime {
+//	int hh, mm, ss;
+//	int tss;
+//public:
+//	MyTime();
+//	MyTime(int h, int m, int s);
+//	void showme();
+//	MyTime Cvt_S2H(int n);
+//	MyTime operator + (MyTime t);
+//	MyTime operator - (MyTime t);
+//	MyTime operator * (int n);
+//	friend MyTime operator * (int n, MyTime t);
+//	MyTime operator / (int n);
+//};
+//int main(void) {
+//	MyTime result;
+//	MyTime t1(7, 29, 48); t1.showme();
+//	MyTime t2(4, 54, 39); t2.showme();
+//
+//	result = t1 + t1 + t1; result.showme();
+//	result = t2 - t1 + t1; result.showme();
+//	result = t1 * 3; result.showme();
+//	result = 3 * t1; result.showme();
+//	result = t2 / 4; result.showme();
+//	result = result.Cvt_S2H(300); result.showme();
+//}
+//MyTime::MyTime() {
+//	hh = mm = ss = tss = 0;
+//}
+//MyTime::MyTime(int h, int m, int s) {
+//	hh = h; mm = m; ss = s;
+//	tss = h * 60 * 60 + m * 60 + s;
+//}
+//void MyTime::showme() {
+//	cout.width(3); cout << hh << " :";
+//	cout.width(3); cout << mm << " :";
+//	cout.width(3); cout << ss << "\t";
+//	cout.width(5); cout << tss << endl;
+//}
+//MyTime MyTime::Cvt_S2H(int tt) {
+//	tt = abs(tt);
+//	MyTime result(tt / 60 / 60, tt / 60 % 60, tt % 60);
+//	return result;
+//}
+//MyTime MyTime::operator + (MyTime t) {
+//	return Cvt_S2H(tss - t.tss);
+//}
+//MyTime MyTime::operator - (MyTime t) {
+//	return Cvt_S2H(tss - t.tss);
+//}
+//MyTime MyTime::operator * (int n) {
+//	return Cvt_S2H(tss * n);
+//}
+//MyTime operator * (int n, MyTime t) {
+//	return t * n;
+//}
+//MyTime MyTime::operator / (int n) {
+//	return Cvt_S2H(tss / n);
+//}
+
+
+
 #include <iostream>
 using namespace std;
 
-class MyTime {
-	int hh, mm, ss;
-	int tss;
+class Complex {
+	double real;
+	double imag;
 public:
-	MyTime();
-	MyTime(int h, int m, int s);
+	Complex();
+	Complex(double real, double imag);
 	void showme();
-	MyTime Cvt_S2H(int n);
-	MyTime operator + (MyTime t);
-	MyTime operator - (MyTime t);
-	MyTime operator * (int n);
-	friend MyTime operator * (int n, MyTime t);
-	MyTime operator / (int n);
+	Complex operator + (Complex& c);
+	Complex operator - (Complex& c);
+	Complex operator * (Complex& c);
+	Complex operator * (double d);
+	Complex operator / (Complex& c);
 };
-int main(void) {
-	MyTime result;
-	MyTime t1(7, 29, 48); t1.showme();
-	MyTime t2(4, 54, 39); t2.showme();
 
-	result = t1 + t1 + t1; result.showme();
-	result = t2 - t1 + t1; result.showme();
-	result = t1 * 3; result.showme();
-	result = 3 * t1; result.showme();
-	result = t2 / 4; result.showme();
-	result = result.Cvt_S2H(300); result.showme();
+Complex Complex::operator + (Complex& c) {
+	return Complex(real + c.real, imag + c.imag);
 }
-MyTime::MyTime() {
-	hh = mm = ss = tss = 0;
+
+Complex Complex::operator - (Complex& c) {
+	return Complex(real - c.real, imag - c.imag);
 }
-MyTime::MyTime(int h, int m, int s) {
-	hh = h; mm = m; ss = s;
-	tss = h * 60 * 60 + m * 60 + s;
+
+Complex Complex::operator * (Complex& c) {
+	return Complex(real * c.real - imag * c.imag, real * c.imag + imag * c.real);
 }
-void MyTime::showme() {
-	cout.width(3); cout << hh << " :";
-	cout.width(3); cout << mm << " :";
-	cout.width(3); cout << ss << "\t";
-	cout.width(5); cout << tss << endl;
+
+Complex Complex::operator * (double d) {
+	return Complex(real * d, imag * d);
 }
-MyTime MyTime::Cvt_S2H(int tt) {
-	tt = abs(tt);
-	MyTime result(tt / 60 / 60, tt / 60 % 60, tt % 60);
-	return result;
+
+Complex Complex::operator / (Complex& c) {
+	Complex conju(c.real, -c.imag);
+	double d = 1 / (c.real * c.real + c.imag * c.imag);
+
+	Complex tmp(real, imag);
+	tmp = tmp.operator * (conju);
+
+	return tmp.operator * (d);
 }
-MyTime MyTime::operator + (MyTime t) {
-	return Cvt_S2H(tss - t.tss);
+
+Complex::Complex() {
+	real = imag = 0.;
 }
-MyTime MyTime::operator - (MyTime t) {
-	return Cvt_S2H(tss - t.tss);
+
+Complex::Complex(double re, double im) {
+	real = re;
+	imag = im;
 }
-MyTime MyTime::operator * (int n) {
-	return Cvt_S2H(tss * n);
+
+
+void Complex::showme() {
+	cout << "(" << real << ", " << imag << "i) \n";
 }
-MyTime operator * (int n, MyTime t) {
-	return t * n;
-}
-MyTime MyTime::operator / (int n) {
-	return Cvt_S2H(tss / n);
+
+int main() {
+	Complex result;
+	Complex c1(2, 3);
+	Complex c2(3, -2);
+
+	result.showme();
+	c1.showme();
+	c2.showme();
+	result = c1 + c2;	result.showme();
+	result = c1 - c2;	result.showme();
+	result = c1 * c2;	result.showme();
+	result = c1 / c2;	result.showme();
 }
