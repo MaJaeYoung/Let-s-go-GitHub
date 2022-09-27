6e6ac9aacc723264c07fa11152d77a73c6b93a81
MaJaeYoung <dabeta1234@gmail.com>
2022-09-27 19:33:23 +0900

    Data structure Language Basics
    c

---
+++ 자료구조기초반/자구기초c.cpp
@@ -0,0 +1,92 @@
+//코드업 1366
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main(void) {
+//	int n;
+//	scanf("%d", &n);
+//	if (n % 2 != 0) {
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 0; j < n; j++) {
+//				if (i == 0 || j==0 || i==n-1 || j == n-1 || i==j || (i+j)==n-1 || i == n/2 || j == n/2) {
+//					printf("*");
+//				}
+//				else {
+//					printf(" ");
+//				}
+//			}
+//			printf("\n");
+//		}
+//	}
+//
+//
+//	return 0;
+//}
+
+//코드업 1355
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main(void) {
+//	int n;
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n;j++) {
+//			if (j >= i) {
+//				printf("*");
+//			}	
+//			else {
+//				printf(" ");
+//			}
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+//코드업 1511
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main(void) {
+//	int num = 1, n, add=0;
+//	scanf("%d", &n);
+//	printf("\n");
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n; j++) {
+//			printf("%d ", num);
+//			if (i==0 || j==0 || i==n-1 || j==n-1) {
+//				add += num;
+//			}
+//			num++;
+//		}
+//		printf("\n\n");
+//	}
+//
+//	printf("%d", add);
+//
+//	return 0;
+//}
+
+
+// 2차원 배열
+//코드업 1508
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(void) {
+
+	int arr[10][10];
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			scanf()
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < n; j++) {
+
+		}
+		printf("\n");
+	}
+
+
+
+	return 0;
+}
