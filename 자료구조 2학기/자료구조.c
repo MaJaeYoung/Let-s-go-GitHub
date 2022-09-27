7856921562e780db779ba83a3c26c4ccf770baa7
MaJaeYoung <dabeta1234@gmail.com>
2022-09-27 19:32:29 +0900

    Data structure1
    c

--- 자료구조 2학기/자료구조.c
+++ 자료구조 2학기/자료구조.c
@@ -392,175 +392,46 @@
 
 
 //쉘 정렬
-//#include<stdio.h>
-//#include<stdlib.h>
-//#define MAX_SIZE 10
-//
-//int arr[MAX_SIZE];
-//
-//void insertion_sort(int arr[], int first, int last, int gap) {
-//	int i, j, key;
-//	for (i = first + gap; i <= last; i= i+gap ) { /*i는 어떻게 증가해야 할까요 ?*/
-//		key = arr[i];
-//		for (j = i - gap; j >= 0 && arr[j] > key; j = j - gap) {
-//			arr[j+gap] = arr[j];					//원래는 j+1에 j를 넣었지만 지금은 gap만큼 뒤로 밀린 곳에 삽입해야한다.
-//		}
-//		arr[j+gap] = key;						//마찬가지로 j+1에 key를 넣는 것이 아닌 gap만큼 더한 곳에 삽입해야한다.
-//	}
-//}
-//
-//void shell_sort(int arr[], int n) {
-//	int i, gap;
-//	for (gap = n / 2; gap>=1; gap = gap/2)	 /*//간격 정하기 gap 변수 활용*/ {
-//		if (gap % 2 == 0) {
-//				gap++;						//gap이 짝수일 때 1을 더하는 것이 좋은 것으로 분석되었다.
-//		}
-//		for (i = 0; i < gap; i++) {
-//			insertion_sort(arr, i, n-1, gap);
-//		}
-//	}
-//}
-//
-//int main() {
-//	int n = MAX_SIZE;
-//	srand(time(NULL));
-//	for (int i = 0; i < n; i++) {
-//		arr[i] = rand() % 100;
-//	}
-//
-//	shell_sort(arr, n);
-//
-//	for (int i = 0; i < n; i++) {
-//		printf("%d ", arr[i]);
-//	}
-//	return 0;
-//}
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_SIZE 10
 
+int arr[MAX_SIZE];
 
+void insertion_sort(int arr[], int first, int last, int gap) {
+	int i, j, key;
+	for (i = first + gap; i <= last; i= i+gap ) { /*i는 어떻게 증가해야 할까요 ?*/
+		key = arr[i];
+		for (j = i - gap; j >= 0 && arr[j] > key; j = j - gap) {
+			arr[j+gap] = arr[j];					//원래는 j+1에 j를 넣었지만 지금은 gap만큼 뒤로 밀린 곳에 삽입해야한다.
+		}
+		arr[j+gap] = key;						//마찬가지로 j+1에 key를 넣는 것이 아닌 gap만큼 더한 곳에 삽입해야한다.
+	}
+}
 
-//퀵정렬
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include<time.h>
-// 
-//#define max_size 10
-//int arr[max_size];
-//
-//int partition(int left, int right) {
-//	int pivot = arr[left];						// 기준값을 뭘로 정할까요?
-//	int low = left + 1;						// 초기화
-//	int high = right;						// 초기화
-//	int temp;
-//	do {
-//		while (arr[low] < pivot				/*어떤 조건이 들어갈까요 ?*/) {
-//			low++;
-//		}
-//		while (arr[high] > pivot			/*어떤 조건이 들어갈까요 ?*/) {
-//			high--;
-//		}
-//		if (low < high) {
-//			temp = arr[low];				// 값 교환
-//			arr[low] = arr[high];
-//			arr[high] = temp;
-//		}
-//	} while (low < high);
-//
-//											// 값 교환(어떤 값들을 바꿔줘야할까요?)
-//	arr[left] = arr[high];
-//	arr[high] = pivot;
-//
-//	return high;							// 어떤 값을 반환해야 할까요?
-//}
-//
-//void quick_sort(int left, int right) {
-//	// left는 정렬해야 할 배열의 첫 인덱스 값, right는 마지막 인덱스 값
-//	if (left < right) {
-//		int q = partition(left, right);		//partition함수는 피봇의 위치(인덱스)를 반환
-//		quick_sort(left, q - 1);					/*왼쪽 리스트 매개변수는 ?*/
-//		quick_sort(q + 1, right);			// 오른쪽 리스트 매개변수는?
-//	}
-//}
-//
-//int main() {
-//	srand(time(NULL));
-//	int n = max_size;
-//	for (int i = 0; i < n; i++) {
-//		arr[i] = rand() % 100;
-//	}
-//
-//	for (int i = 0; i < n; i++) {
-//		printf("%d ", arr[i]);
-//	}
-//
-//	printf("\n\n");
-//
-//	quick_sort(0, n - 1);
-//
-//	for (int i = 0; i < n; i++) {
-//		printf("%d ", arr[i]);
-//	}
-//
-//	return 0;
-//}
+void shell_sort(int arr[], int n) {
+	int i, gap;
+	for (gap = n / 2; gap>=1; gap = gap/2)	 /*//간격 정하기 gap 변수 활용*/ {
+		if (gap % 2 == 0) {
+				gap++;						//gap이 짝수일 때 1을 더하는 것이 좋은 것으로 분석되었다.
+		}
+		for (i = 0; i < gap; i++) {
+			insertion_sort(arr, i, n-1, gap);
+		}
+	}
+}
 
+int main() {
+	int n = MAX_SIZE;
+	srand(time(NULL));
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 100;
+	}
 
-//봉식이꺼
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include<time.h>
-//
-//#define MAX_SIZE 10
-//int arr[MAX_SIZE];
-//
-//////   퀵 정렬
-//
-//int partition(int left, int right) {
-//   int pivot = arr[left];
-//   int low = left + 1;
-//   int high = right;
-//   int temp;
-//   do {
-//      while (pivot > arr[low]) {
-//         low++;
-//      } 
-//      while (pivot < arr[high]){
-//         high--;
-//      }
-//      if (low < high){
-//         temp = arr[low];
-//         arr[low] = arr[high];
-//         arr[high] = temp;
-//      }
-//   } while ( low < high);
-//   arr[left] = arr[high];
-//   arr[high] = pivot;
-//   return high;
-//}
-//
-//void quick_sort(int left, int right) {
-//   if (left < right){
-//      int q = partition(left, right);
-//      quick_sort(left, q - 1);
-//      quick_sort(q + 1, right);
-//   }
-//}
-//
-//int main() {
-//   srand(time(NULL));
-//   int n = MAX_SIZE;
-//   for (int i = 0; i < n; i++)
-//      arr[i] = rand() % 100;
-//   for (int i = 0; i < n; i++)
-//      printf("%d ", arr[i]);
-//
-//      printf("\n\n");
-//
-//   quick_sort(0, n - 1);
-//   for (int i = 0; i < n; i++)
-//      printf("%d ", arr[i]);
-//   return 0;
-//}
+	shell_sort(arr, n);
 
-
-
-// 
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
