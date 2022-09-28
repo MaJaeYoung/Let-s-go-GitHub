e122b2c7064e9d22d2bf7bf9c01a7d54712a65af
MaJaeYoung <dabeta1234@gmail.com>
2022-09-28 22:12:54 +0900

    Data structure Language Basics
    c

---
+++ 자료구조기초반/자구기초.c
@@ -0,0 +1,220 @@
+//코드업 1366
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main(void) {
+//	int n;
+//	scanf("%d", &n);
+//	if (n % 2 != 0) {
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 0; j < n; j++) {
+//				if (i == 0 || j==0 || i==n-1 || j == n-1 || i==j || (i+j)==n-1 || i == n/2 || j == n/2) {
+//					printf("*");
+//				}
+//				else {
+//					printf(" ");
+//				}
+//			}
+//			printf("\n");
+//		}
+//	}
+//
+//
+//	return 0;
+//}
+
+//코드업 1355
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main(void) {
+//	int n;
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n;j++) {
+//			if (j >= i) {
+//				printf("*");
+//			}	
+//			else {
+//				printf(" ");
+//			}
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+//코드업 1511
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main(void) {
+//	int num = 1, n, add=0;
+//	scanf("%d", &n);
+//	printf("\n");
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n; j++) {
+//			printf("%d ", num);
+//			if (i==0 || j==0 || i==n-1 || j==n-1) {
+//				add += num;
+//			}
+//			num++;
+//		}
+//		printf("\n\n");
+//	}
+//
+//	printf("%d", add);
+//
+//	return 0;
+//}
+
+
+// 2차원 배열
+//코드업 1508
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//
+//int main(void) {
+//
+//	int arr[20][20];
+//	int n;
+//	int i = 0;
+//	scanf("%d", &n);
+//
+//	for (int i = 0; i < n; i++) {
+//			scanf("%d", &arr[i][0]);
+//	}
+//	
+//	printf("\n");         //답 넣을때는 빼기
+//
+//	for (int i = 1; i < n; i++) {
+//		for (int j = 1; j < n;j++) {
+//			arr[i][j] = arr[i][j-1]-arr[i-1][j-1];
+//		}
+//	}
+//
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j <= i; j++) {
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//		
+//	return 0;
+//}
+
+//삽입
+//#include<stdio.h>
+//#include<stdlib.h>
+//#define MAX_SIZE 10
+//
+//int arr[MAX_SIZE];
+//void insertion_sort() {
+//	int key, i, j;
+//	for (i = 1; i < MAX_SIZE; i++) { // i가 1부터 시작하는 것에 유의
+//		key = arr[i];
+//		for (j = i - 1; j >= 0 && arr[j] > key; j--) { // key 값의 위치 찾기
+//			arr[j + 1] = arr[j];
+//		} 
+//		arr[j + 1] = key;	// key 값 삽입
+//	}
+//} // <<-- 이거 없었음조심
+//int main() {
+//	srand(time(NULL));
+//	int n = MAX_SIZE;
+//	for (int i = 0; i < n; i++) {
+//		arr[i] = rand() % 100;
+//	}
+//	insertion_sort();
+//	for (int i = 0; i < n; i++) {
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//버블
+//#include<stdio.h>
+//#include<stdlib.h>
+//#define MAX_SIZE 10
+//
+//int arr[MAX_SIZE];
+//void Bubble_sort() {
+//	int temp;
+//	for (int i = MAX_SIZE-1; i >=1 ; i--) { // 총 단계는 배열 사이즈-1 수행됨
+//		for (int j = 0; j <= i-1; j++) { // 뒤에서부터 거품처럼 정렬되는 구조
+//			if (arr[j] > arr[j + 1]) {
+//				temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;
+//			}
+//		}
+//	}
+//}
+//
+//int main() {
+//	srand(time(NULL));
+//	int n = MAX_SIZE;
+//	for (int i = 0; i < n; i++) {
+//		arr[i] = rand() % 100;
+//	}
+//
+//	Bubble_sort();
+//
+//	for (int i = 0; i < n; i++) {
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//} 
+
+
+
+//퀵
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_SIZE 10
+int arr[MAX_SIZE];
+int partition(int left, int right) {
+	int pivot = arr[left];
+	int low = left + 1;
+	int high = right;
+	int temp;
+	do {
+		while (arr[low] < pivot) {
+			low++;
+		}
+		while (arr[high] > pivot) {
+			high--;
+		}
+		if (low < high) {
+			// 값 교환
+			temp = arr[low];
+			arr[low] = arr[high];
+			arr[high] = temp;
+		}
+	} while (low < high);
+
+	// 값 교환(어떤 값들을 바꿔줘야할까요?)
+	temp = arr[left];
+	arr[left] = arr[high];
+	arr[high] = pivot;
+	return high;
+}
+
+void quick_sort(int left, int right) {
+	// left는 정렬해야 할 배열의 첫 인덱스 값, right는 마지막 인덱스 값
+	if (left < right) {
+		int q = partition(left, right); // partition함수는 피봇의 위치(인덱스)를 반환
+		quick_sort(left, q - 1);
+		quick_sort(q + 1, right); // 오른쪽 리스트 매개변수는?
+	}
+}
+
+int main() {
+	srand(time(NULL));
+	int n = MAX_SIZE;
+	for (int i = 0; i < n; i++)
+		arr[i] = rand() % 100;
+
+	quick_sort(0, n - 1);
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+	return 0;
+}
