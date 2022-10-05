cc296ed6e01af2ceaa3c24ad88bc8f10fd9a42f4
MaJaeYoung <dabeta1234@gmail.com>
2022-10-05 20:26:20 +0900

    Data structure Language Basics
    c

--- 자료구조기초반/자구기초.c
+++ 자료구조기초반/자구기초.c
@@ -167,54 +167,179 @@
 
 
 //퀵
-#include<stdio.h>
-#include<stdlib.h>
-#define MAX_SIZE 10
-int arr[MAX_SIZE];
-int partition(int left, int right) {
-	int pivot = arr[left];
-	int low = left + 1;
-	int high = right;
-	int temp;
-	do {
-		while (arr[low] < pivot) {
-			low++;
-		}
-		while (arr[high] > pivot) {
-			high--;
-		}
-		if (low < high) {
-			// 값 교환
-			temp = arr[low];
-			arr[low] = arr[high];
-			arr[high] = temp;
-		}
-	} while (low < high);
+//#include<stdio.h>
+//#include<stdlib.h>
+//#define MAX_SIZE 10
+//int arr[MAX_SIZE];
+//int partition(int left, int right) {
+//	int pivot = arr[left];
+//	int low = left + 1;
+//	int high = right;
+//	int temp;
+//	do {
+//		while (arr[low] < pivot) {
+//			low++;
+//		}
+//		while (arr[high] > pivot) {
+//			high--;
+//		}
+//		if (low < high) {
+//			// 값 교환
+//			temp = arr[low];
+//			arr[low] = arr[high];
+//			arr[high] = temp;
+//		}
+//	} while (low < high);
+//
+//	// 값 교환(어떤 값들을 바꿔줘야할까요?)
+//	temp = arr[left];
+//	arr[left] = arr[high];
+//	arr[high] = pivot;
+//	return high;
+//}
+//
+//void quick_sort(int left, int right) {
+//	// left는 정렬해야 할 배열의 첫 인덱스 값, right는 마지막 인덱스 값
+//	if (left < right) {
+//		int q = partition(left, right); // partition함수는 피봇의 위치(인덱스)를 반환
+//		quick_sort(left, q - 1);
+//		quick_sort(q + 1, right); // 오른쪽 리스트 매개변수는?
+//	}
+//}
+//
+//int main() {
+//	srand(time(NULL));
+//	int n = MAX_SIZE;
+//	for (int i = 0; i < n; i++)
+//		arr[i] = rand() % 100;
+//
+//	quick_sort(0, n - 1);
+//	for (int i = 0; i < n; i++)
+//		printf("%d ", arr[i]);
+//	return 0;
+//}
 
-	// 값 교환(어떤 값들을 바꿔줘야할까요?)
-	temp = arr[left];
-	arr[left] = arr[high];
-	arr[high] = pivot;
-	return high;
-}
 
-void quick_sort(int left, int right) {
-	// left는 정렬해야 할 배열의 첫 인덱스 값, right는 마지막 인덱스 값
-	if (left < right) {
-		int q = partition(left, right); // partition함수는 피봇의 위치(인덱스)를 반환
-		quick_sort(left, q - 1);
-		quick_sort(q + 1, right); // 오른쪽 리스트 매개변수는?
-	}
-}
 
-int main() {
-	srand(time(NULL));
-	int n = MAX_SIZE;
-	for (int i = 0; i < n; i++)
-		arr[i] = rand() % 100;
+//스택 알고리즘
+//#include <stdio.h>
+//int stack[100];
+//int top = -1;
+//
+//int is_empty() {
+//	if (top == -1) {
+//		return 1;
+//	}
+//	else {
+//		return 0;
+//	}
+//}
+//
+//int is_full() {
+//	if (top >= 99) {
+//		return 1;
+//	}
+//	else {
+//		return 0;
+//	}
+//}
+//
+//int push(int item) {
+//	if (is_full()) {
+//		printf("error");
+//	}
+//	else {
+//		stack[++top] = item;
+//	}
+//}
+//
+//int pop() {
+//	if (is_empty()) {
+//		printf("error");
+//		return 0;
+//	}
+//	else {
+//		return stack[top--];
+//	}
+//}
+//
+//int main() {
+//	push(1);
+//	printf("Stack : ");
+//	for (int i = 0; i < 10; i++) {
+//		printf("%d ", stack[i]);
+//	}
+//
+//	push(2);
+//	printf("\n\nStack : ");
+//	for (int i = 0; i < 10; i++) {
+//		printf("%d ", stack[i]);
+//	}
+//
+//	push(3);
+//	printf("\n\nStack : ");
+//	for (int i = 0; i < 10; i++) {
+//		printf("%d ", stack[i]);
+//	}
+//	return 0;
+//}
 
-	quick_sort(0, n - 1);
-	for (int i = 0; i < n; i++)
-		printf("%d ", arr[i]);
-	return 0;
-}
+//코드업 1402
+//#include <stdio.h>
+//int stack[100];
+//int top = -1;
+//
+//int is_empty() {
+//	if (top == -1) {
+//		return 1;
+//	}
+//	else {
+//		return 0;
+//	}
+//}
+//
+//int is_full() {
+//	if (top >= 99) {
+//		return 1;
+//	}
+//	else {
+//		return 0;
+//	}
+//}
+//
+//int push(int item) {
+//	if (is_full()) {
+//		printf("error");
+//	}
+//	else {
+//		stack[++top] = item;
+//	}
+//}
+//
+//int pop() {
+//	if (is_empty()) {
+//		printf("error");
+//		return 0;
+//	}
+//	else {
+//		return stack[top--];
+//	}
+//}
+//
+//int main() {
+//	int n,num;
+//	scanf("%d", &n);
+//	if (n <= 1000) {
+//		for (int i = 0; i < n; i++) {
+//		scanf("%d", &num);
+//		push(num);
+//		} 
+//	}
+//
+//	for (int i = 0; i < n; i++) {
+//		printf("%d ", pop());
+//	}
+//
+//	return 0;
+//}
+
