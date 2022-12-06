e3df6d5dee74eadff0fd75acec8198f5b9cc0e40
MaJaeYoung <dabeta1234@gmail.com>
2022-12-06 16:58:01 +0900

    cpp class 1
    c++

--- C++ 시작/소스1.cpp
+++ C++ 시작/소스1.cpp
@@ -486,70 +486,70 @@
 //	result = c1 / c2;	result.showme();
 //}
 
-// 수행평가
-//#include <iostream>          
-//using namespace std;
-//
-//class Complex209 {
-//	double real;
-//	double imag;
-//public:
-//	Complex209();
-//	Complex209(double real, double imag);
-//	void showme();
-//	Complex209 operator + (Complex209& c);
-//	Complex209 operator - (Complex209& c);
-//	Complex209 operator * (Complex209& c);
-//	Complex209 operator * (double d);
-//	Complex209 operator / (Complex209& c);
-//};
-//
-//Complex209 Complex209::operator + (Complex209& c) {
-//	return Complex209(real + c.real, imag + c.imag);
-//}
-//
-//Complex209 Complex209::operator - (Complex209& c) {
-//	return Complex209(real - c.real, imag - c.imag);
-//}
-//
-//Complex209 Complex209::operator * (Complex209& c) {
-//	return Complex209(real * c.real - imag * c.imag, real * c.imag + imag * c.real);
-//}
-//
-//Complex209 Complex209::operator * (double d) {
-//	return Complex209(real * d, imag * d);
-//}
-//
-//Complex209 Complex209::operator / (Complex209& c) {
-//	Complex209 conju(c.real, -c.imag);
-//	double d = 1 / (c.real * c.real + c.imag * c.imag);
-//
-//	Complex209 tmp(real, imag);
-//	tmp = tmp.operator * (conju);
-//
-//	return tmp.operator * (d);
-//}
-//
-//Complex209::Complex209() {
-//	real = imag = 0.;
-//}
-//
-//Complex209::Complex209(double re, double im) {
-//	real = re;
-//	imag = im;
-//}
-//
-//void Complex209::showme() {
-//	cout << "(" << real << ", " << imag << "i) \n";
-//}
-//
-//int main() {
-//	Complex209 result;
-//	Complex209 c1(5, 9);
-//	Complex209 c2(1, -6);
-//
-//	result = c1 + c2;	result.showme();
-//	result = c1 - c2;	result.showme();
-//	result = c1 * c2;	result.showme();
-//	result = c1 / c2;	result.showme();
-//}
+// 수행평가  기수 정렬
+#include <iostream>          
+using namespace std;
+
+class Complex209 {
+	double real;
+	double imag;
+public:
+	Complex209();
+	Complex209(double real, double imag);
+	void showme();
+	Complex209 operator + (Complex209& c);
+	Complex209 operator - (Complex209& c);
+	Complex209 operator * (Complex209& c);
+	Complex209 operator * (double d);
+	Complex209 operator / (Complex209& c);
+};
+
+Complex209 Complex209::operator + (Complex209& c) {
+	return Complex209(real + c.real, imag + c.imag);
+}
+
+Complex209 Complex209::operator - (Complex209& c) {
+	return Complex209(real - c.real, imag - c.imag);
+}
+
+Complex209 Complex209::operator * (Complex209& c) {
+	return Complex209(real * c.real - imag * c.imag, real * c.imag + imag * c.real);
+}
+
+Complex209 Complex209::operator * (double d) {
+	return Complex209(real * d, imag * d);
+}
+
+Complex209 Complex209::operator / (Complex209& c) {
+	Complex209 conju(c.real, -c.imag);
+	double d = 1 / (c.real * c.real + c.imag * c.imag);
+
+	Complex209 tmp(real, imag);
+	tmp = tmp.operator * (conju);
+
+	return tmp.operator * (d);
+}
+
+Complex209::Complex209() {
+	real = imag = 0.;
+}
+
+Complex209::Complex209(double re, double im) {
+	real = re;
+	imag = im;
+}
+
+void Complex209::showme() {
+	cout << "(" << real << ", " << imag << "i) \n";
+}
+
+int main() {
+	Complex209 result;
+	Complex209 c1(5, 9);
+	Complex209 c2(1, -6);
+
+	result = c1 + c2;	result.showme();
+	result = c1 - c2;	result.showme();
+	result = c1 * c2;	result.showme();
+	result = c1 / c2;	result.showme();
+}
--- C++ 시작/소스func.cpp
+++ C++ 시작/소스func.cpp
@@ -5,6 +5,7 @@
 //        cout.width(3); cout << *(p + a);
 //    } cout << "\n\n";
 //}
+// 
 //Node* insertNode(Node* p, int tmp) {
 //    Node* newNode = new Node;
 //
@@ -17,6 +18,7 @@
 //
 //    return p;
 //}
+// 
 //void In_order(Node* p) {
 //    if (p) {
 //        In_order(p->l_link);
@@ -24,9 +26,53 @@
 //        In_order(p->r_link);
 //    }
 //}
+// 
 //Node* Search(Node* p, int tmp) {
 //    if (p == NULL) return NULL;
 //    if (tmp == p->item) return p;
 //    else if (tmp < p->item) return Search(p->l_link, tmp);
 //    else return Search(p->r_link, tmp);
+//}
+
+
+
+//c++¹öÀü 
+//#include "bst.h"
+//
+//void p_out(int* p, int MAX) {
+//    for (int a = 0; a < MAX; ++a) {
+//        cout.width(3); cout << *(p + a);
+//    } cout << "\n\n";
+//}
+//
+//BST* BST::insertNode(int tmp) {
+//
+//    if (this == NULL) {
+//        BST* newNode = new BST(tmp);
+//            return newNode;
+//    }
+//    else if (tmp < item) l_link = l_link -> insertNode(tmp);
+//    else if (tmp > item) r_link = r_link -> insertNode(tmp);
+//
+//    return this;
+//}
+//
+//void BST::In_order() {
+//    if (this) {
+//        l_link->In_order();
+//        cout.width(3); 
+//        cout << item;
+//        r_link->In_order();
+//    }
+//}
+//
+//BST* BST::Search(int tmp) {
+//    if (this == NULL) 
+//        return NULL;
+//    if (tmp == item) 
+//        return this;
+//    else if (tmp < item) 
+//        return l_link -> Search(tmp);
+//    else 
+//        return r_link -> Search(tmp);
 //}
